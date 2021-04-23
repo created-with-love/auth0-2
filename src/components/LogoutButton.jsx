da500e6216a3e376f62bc94e7b776fc3db7339ca
Vladymyr Ivanchenko <vladymyr_ivanchenko@bigmir.net>
2021-04-23 15:35:15 +0300

    fix logOut

--- src/components/LogoutButton.jsx
+++ src/components/LogoutButton.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
-import { useHistory } from 'react-router-dom';
+
 import { useAuth0 } from '@auth0/auth0-react';
 
 const LogoutButton = () => {
   const { logout, isAuthenticated } = useAuth0();
-  const history = useHistory();
 
-  const logOut = () => {
-    logout();
-    history.push('/auth0-2');
-  };
-  return isAuthenticated && <button onClick={logOut}>Log Out</button>;
+  console.log(window.location.origin.slice(7));
+
+  return (
+    isAuthenticated && (
+      <button
+        onClick={() =>
+          logout({ returnTo: `${window.location.origin}/auth0-2` })
+        }
+      >
+        Log Out
+      </button>
+    )
+  );
 };
 
 export default LogoutButton;
